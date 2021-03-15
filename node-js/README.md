b8525ac97ebbb99fe361bc41ea2f6314b1a6cca9
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-15 18:38:48 -0400

    Create examples for nodejs

---
+++ node-js/README.md
@@ -0,0 +1,20 @@
+# NodeJS @fullexpression/confusion-matrix-stats examples
+
+NodeJS project containing a set of examples on how to consume @fullexpression/confusion-matrix-stats.
+
+All examples are written in [index.js](./index.js).
+
+## 🛠️ How to setup
+
+Simple run the following command:
+
+`npm install`
+
+
+## 👩‍💻 How to run
+
+*If you not have a 14 or higher node version is required, please, install or update it from [here](https://nodejs.org/en/).*
+
+Run the following command:
+
+`npm run build`
---
+++ node-js/index.js
@@ -0,0 +1,17 @@
+const { ConfusionMatrix, AverageMethod } = require('@fullexpression/confusion-matrix-stats');
+
+const confusionMatrix = new ConfusionMatrix({
+    labels: ["Happiness", "Sadness", "Disgust"], 
+    matrix: [[50, 2, 3],
+             [8, 50 ,5],
+             [2, 5 ,50]]
+});
+
+// Calculates the accuracy value for the all matrix.
+const accuracy = confusionMatrix.accuracy();
+console.log(`The accuracy value is: ${accuracy}`);
+
+// Calculates the f1Score, only for "sadness" class, using the Macro average method.
+const labelAccuracy = confusionMatrix.f1Score({ label: "Sadness", average: AverageMethod.Macro});
+console.log(`The f1Score for 'Sadness' is ${labelAccuracy}`)
+
