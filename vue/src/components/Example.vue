8ae58e49dde60ff653ccae8e16f5bffa0dbd7a14
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-15 22:28:11 -0400

    Create Vue example.

---
+++ vue/src/components/Example.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>Accuracy value for the all matrix:</h1>
+    <h2>{{ accuracy }}</h2>
+    <br>
+    <h1>F1Score, only for "sadness" class, using the Macro average method:</h1>
+    <h2>{{ f1score }}</h2>
+  </div>
+</template>
+
+<script>
+import { ConfusionMatrix, AverageMethod } from '@fullexpression/confusion-matrix-stats';
+
+const confusionMatrix = new ConfusionMatrix({
+        labels: ["Happiness", "Sadness", "Disgust"],
+        matrix: [[50, 2, 3],
+        [8, 50, 5],
+        [2, 5, 50]]
+    });
+
+export default {
+  name: 'Example',
+  data () {
+      return {
+          accuracy: confusionMatrix.accuracy(),
+          f1score: confusionMatrix.f1Score({ label: "Sadness", average: AverageMethod.Macro })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
