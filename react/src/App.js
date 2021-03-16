26ca4015079f53311e9659dad0c872e75e71b1b8
RicardoGomesRocha <ricardo1992rocha@gmail.com>
2021-03-15 22:03:22 -0400

    Add example to react application.

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { ConfusionMatrix, AverageMethod } from '@fullexpression/confusion-matrix-stats';
+
+const confusionMatrix = new ConfusionMatrix({
+    labels: ["Happiness", "Sadness", "Disgust"], 
+    matrix: [[50, 2, 3],
+             [8, 50 ,5],
+             [2, 5 ,50]]
+});
+
 
 function App() {
+  const accuracy = confusionMatrix.accuracy();
+  const f1Score = confusionMatrix.f1Score({ label: "Sadness", average: AverageMethod.Macro })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label class="description">Accuracy value for the all matrix:</label>
+        <label class="value">{accuracy}</label>
+        <br></br>
+        <label class="description">F1Score, only for "sadness" class, using the Macro average method:</label>
+        <label class="value">{f1Score}</label>
       </header>
     </div>
   );
